f844bc941f49378372e21e4da09f4b165715d251
barnabas.novak12 <barnabas.novak12@gmail.com>
2019-11-29 10:58:34 +0100

    mentés

--- UserMaitenance/Form1.cs
+++ UserMaitenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace UserMaitenance
             InitializeComponent();
             
             textBox1.Text = Resource1.FullName; // label1
-           
+            button2.Text = Resource1.Write;
             button1.Text = Resource1.Add; // button1
 
             listBox1.DataSource = users;
@@ -39,7 +40,21 @@ namespace UserMaitenance
 
         private void Button2_Click(object sender, EventArgs e)
         {
-
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+               
+                foreach (var s in users)
+                {
+                   
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.Write(";");
+                    
+                }
+            }
         }
     }
 }
